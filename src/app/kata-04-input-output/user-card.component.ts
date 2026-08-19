84404eb72c439b27335e697c5aef6700c82cdfa7
Fernando Espana <f.espana.cordoba@gmail.com>
2026-08-18 19:15:40 -0500

    Kata Number four terminated

--- src/app/kata-04-input-output/user-card.component.ts
+++ src/app/kata-04-input-output/user-card.component.ts
@@ -5,13 +5,22 @@ import { User } from './user.model';
   selector: 'app-user-card',
   template: `
     <!-- TODO
-         nombre -> data-testid="user-name"
-         rol    -> data-testid="user-role"
-         botón  -> data-testid="select-btn"
+        nombre -> data-testid="user-name"
+        rol    -> data-testid="user-role"
+        botón  -> data-testid="select-btn"
     -->
+    <span data-testid="user-name">{{user.name}} </span>
+    <span data-testid="user-role">{{user.role}} </span>
+    <button (click)="sendSelected()"data-testid="select-btn">Selected</button>
   `,
 })
 export class UserCardComponent {
   // TODO: declara el @Input y el @Output.
   @Input() user!: User;
+
+  @Output() selected = new EventEmitter<User>();  
+
+  sendSelected() {
+    this.selected.emit(this.user);
+  }
 }
--- src/app/kata-04-input-output/user-list.component.ts
+++ src/app/kata-04-input-output/user-list.component.ts
@@ -1,24 +1,31 @@
 import { Component } from '@angular/core';
 import { User } from './user.model';
+import { UserCardComponent } from './user-card.component';
 
 @Component({
   selector: 'app-user-list',
   template: `
     <!-- TODO
-         un app-user-card por usuario, escuchando su salida
-         nombre seleccionado -> data-testid="selected-name" (ausente si no hay selección)
+        un app-user-card por usuario, escuchando su salida
+        nombre seleccionado -> data-testid="selected-name" (ausente si no hay selección)
     -->
+    <div *ngFor="let user of users, index as i">
+      <app-user-card (selected)="onSelected($event)" [user]="user"></app-user-card>
+    </div>
+    <p *ngIf="selected" data-testid="selected-name">{{selected.name}}</p>
   `,
 })
 export class UserListComponent {
   users: User[] = [
     { id: 1, name: 'Ada', role: 'Engineer' },
     { id: 2, name: 'Grace', role: 'Admiral' },
-    { id: 3, name: 'Linus', role: 'Architect' },
+    { id: 3, name: 'Linus', role: 'Architect' }
   ];
 
   selected: User | null = null;
 
   // TODO
-  onSelected(user: User): void {}
+  onSelected(user: User): void {
+    this.selected = user;
+  }
 }
